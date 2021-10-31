c19b41abd738b43b9da6ca904d345fb71b28aaed
Tanjilul Anwar <36304879+TanjilulAnwar@users.noreply.github.com>
2021-10-31 14:24:53 +0600

    Create BalanceBracketSequence.cpp

---
+++ BalanceBracketSequence.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+stack<char> stk ;
+string s = "()((()())))";
+//cin>>s;
+int len = s.size();
+
+for(int i =0; i< len; i++){
+if(s[i] == '('){
+  stk.push(s[i]);
+}
+else if(s[i] == ')'){
+  stk.pop();
+}
+else{
+//do nothing
+}
+}
+
+
+if(!stk.empty())
+    cout<<"Unbalanced!"<<endl;
+else
+    cout<<"Balanced!"<<endl;
+
+
+return 0;
+}
