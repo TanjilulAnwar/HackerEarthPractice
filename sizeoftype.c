739ff8863150f869d36c25067be13b36f3cfdfd6
Tanjilul Anwar <36304879+TanjilulAnwar@users.noreply.github.com>
2021-11-02 17:06:00 +0600

    Update sizeoftype.c

--- sizeoftype.c
+++ sizeoftype.c
@@ -4,20 +4,19 @@ int main()
 {
   char *ptr;
 	printf("%lu\n", sizeof(*ptr));//size of pointer  value
-  printf("%lu\n",sizeof(ptr));//size of pointer
-
+  printf("%lu\n",sizeof(ptr));//size of pointer allocated to hold the address
 
  int *ptr1;
 	printf("%lu\n", sizeof(*ptr1));//size of pointer  value
-  printf("%lu\n",sizeof(ptr1));//size of pointer
+  printf("%lu\n",sizeof(ptr1));//size of pointer allocated to hold the address
 
   double *ptr2;
 	printf("%lu\n", sizeof(*ptr2));//size of pointer  value
-  printf("%lu\n",sizeof(ptr2));//size of pointer
+  printf("%lu\n",sizeof(ptr2));//size of pointer allocated to hold the address
 
   float *ptr3;
 	printf("%lu\n", sizeof(*ptr3));//size of pointer  value
-  printf("%lu\n",sizeof(ptr3));//size of pointer
+  printf("%lu\n",sizeof(ptr3));//size of pointer allocated to hold the address
 
 
 
